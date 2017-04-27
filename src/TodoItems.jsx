6465fc1e0633b84a0e72102be7d9349f9940db14
Joonas Aalto <joonas.aalto@wunderdog.fi>
2017-04-27 17:53:03 +0300

    Render list of to-do items.

--- src/TodoItems.jsx
+++ src/TodoItems.jsx
@@ -5,15 +5,18 @@ const TodoItem = ({ item, remove }) => {
   // We should call the remove callback when an item is clicked
   return (
     <div className="Todo-item">
+      {item.text}
     </div>
   );
 };
 
 export default ({ items, remove }) => {
-  // We should build a list of item components from the props
-
+  const todoItems = items.map(item => (
+    <TodoItem key={item.id} item={item} remove={remove} />
+  ));
   return (
     <div className="Todo-item-list" style={{ marginTop: "30px" }}>
+      {todoItems}
     </div>
   );
 };
